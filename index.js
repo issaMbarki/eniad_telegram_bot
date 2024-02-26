a9134c34433794508f986cce101811ae6b365ab4
issaMbarki <49issam@gmail.com>
2024-02-26 23:41:26 +0100

    remove unused code

--- index.js
+++ index.js
@@ -43,74 +43,14 @@ bot.on("callback_query", (query) => {
     }
     return;
   }
-  
+
   const documentAction = documentActions[chosenCommand];
   if (documentAction) {
     try {
-      console.log(documentAction.filePath);
       bot.sendDocument(chatId, documentAction.filePath);
       console.log("File sent successfully");
     } catch (error) {
       console.error("Error sending file:", error);
     }
   }
-
-  // switch (chosenCommand) {
-  //   case "s1":
-  //     text = "choose a module: ";
-  //     replyMarkup = {
-  //       inline_keyboard: modules.s1_modules,
-  //     };
-  //     break;
-
-  //   case "s2":
-  //     text = "choose a module: ";
-  //     replyMarkup = {
-  //       inline_keyboard: modules.s2_modules,
-  //     };
-  //     break;
-
-  //   case "ibd":
-  //     text = "choose what you want: ";
-  //     replyMarkup = {
-  //       inline_keyboard: contents.ibd_content,
-  //     };
-  //     break;
-
-  //   case "ibd_courses":
-  //     text = "choose the course you want: ";
-  //     replyMarkup = {
-  //       inline_keyboard: courses.ibd_courses,
-  //     };
-  //     break;
-  //   case "ibd_ch01":
-  //     try {
-  //       bot.sendDocument(chatId, "./ibd01.txt");
-  //       console.log("File sent successfully");
-  //     } catch (error) {
-  //       console.error("Error sending file:", error);
-  //     }
-  //     break;
-  //   case "ibd_ch02":
-  //     try {
-  //       bot.sendDocument(chatId, "./ibd02.txt");
-  //       console.log("File sent successfully");
-  //     } catch (error) {
-  //       console.error("Error sending file:", error);
-  //     }
-  //     break;
-
-  //   default:
-  //     break;
-  // }
-  // if (replyMarkup) {
-  //   bot.editMessageText(text, {
-  //     chat_id: chatId,
-  //     message_id: query.message.message_id,
-  //   });
-  //   bot.editMessageReplyMarkup(
-  //     { inline_keyboard: replyMarkup.inline_keyboard },
-  //     { chat_id: chatId, message_id: query.message.message_id }
-  //   );
-  // }
 });
